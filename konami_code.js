8e470c9b6236d7c0acaf05cd72e542fc0b52c0a6
Arella Gussow <arellaeg@gmail.com>
2018-06-09 08:31:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ let index =0;
 
 function init() {
   document.body.addEventListener('keydown', (event)=> {
-  const key = event.key;
+  const key = event.key);
   if (key===codes[index]){
     index++;
     if (index===codes.length-1){
@@ -24,5 +24,5 @@ function init() {
     }
   } else {
     index=0;
-  })
+  }
 }
